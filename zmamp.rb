d012acccc19febed9dcf01bf2986d33b4b7f0fd2
Akihiro Fukaya <fakihiro1988@gmail.com>
2017-10-21 22:28:28 +0900

    urlを修正

--- zmamp.rb
+++ zmamp.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Zmamp < Formula
   homepage "https://github.com/fakihiro/Zmamp"
-  url "https://github.com/fakihiro/Zmamp/blob/master/Zmamp"
+  url "https://raw.githubusercontent.com/fakihiro/Zmamp/master/Zmamp"
   sha256 "7514ea121d1e5a2614ed6e6ee52a43491a87d83a096afd602e91c9a606ccb1b8"
   version "1.0"
 
