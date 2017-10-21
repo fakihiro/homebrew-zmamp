919d981dfb8fe4e062d580ab3cbef50abc9f3875
Akihiro Fukaya <fakihiro1988@gmail.com>
2017-10-21 22:10:18 +0900

    versionを追加

--- zmamp.rb
+++ zmamp.rb
@@ -4,6 +4,7 @@ class Zmamp < Formula
   homepage "https://github.com/fakihiro/Zmamp"
   url "https://github.com/fakihiro/Zmamp/blob/master/Zmamp"
   sha256 "7514ea121d1e5a2614ed6e6ee52a43491a87d83a096afd602e91c9a606ccb1b8"
+  version "1.0"
 
   def install
     bin.install "Zmamp"
